c32d0a8e31558c37f13d22a6083104e4228c8f5d
HugoNCastro <hugonaaman@gmail.com>
2025-05-16 14:16:27 -0300

    Add bcryptjs

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,5 +1,6 @@
 import { ConflictException, Body, Controller, Post } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -21,11 +22,13 @@ export class CreateAccountController {
       )
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     })
   }
