2b3bd6b5fe7d2e82d80db169f9017f971844ff97
HugoNC <hugo.castro@comunix.net.br>
2025-06-05 10:01:09 -0300

    Alter payload schema
    Conclude create question controller

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,11 +5,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt'
 import { Env } from 'src/env'
 import { z } from 'zod'
 
-const tokenPayloadSchema = z.object({
+const userPayloadSchema = z.object({
   sub: z.string().uuid(),
 })
 
-export type TokenPayload = z.infer<typeof tokenPayloadSchema>
+export type UserPayloadSchema = z.infer<typeof userPayloadSchema>
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -23,7 +23,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     })
   }
 
-  async validate(payload: TokenPayload) {
-    return tokenPayloadSchema.parse(payload)
+  async validate(payload: UserPayloadSchema) {
+    return userPayloadSchema.parse(payload)
   }
 }
