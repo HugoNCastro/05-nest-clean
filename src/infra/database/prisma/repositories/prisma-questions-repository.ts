f8f475127bf9db9de5c8761006cd58006eea2cf3
HugoNCastro <hugonaaman@gmail.com>
2025-07-11 16:43:39 -0300

    Create prisma question mapper and alter question entity prop

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,9 +2,14 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {
+  }
+
   create(question: Question): Promise<void> {
     throw new Error('Method not implemented.')
   }
@@ -13,8 +18,18 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     throw new Error('Method not implemented.')
   }
 
-  findById(questionId: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findById(questionId: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
   findBySlug(slug: string): Promise<Question | null> {
